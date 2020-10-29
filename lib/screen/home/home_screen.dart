62311f27446c11d4dc7174723fb565b09190cec8
fuadreza <fuadreza1011@gmail.com>
2020-10-29 23:23:23 +0700

    Adding counter

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -7,14 +6,85 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int number;
+
+  @override
+  void initState() {
+    super.initState();
+    number = 0;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("SPOTIFUAD"),
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Text('+'),
+          onPressed: () {
+            increaseCount();
+          },
+        ),
+        body: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                number.toString(),
+                style: TextStyle(color: Colors.green[500], fontSize: 21),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              InkWell(
+                onTap: () {
+                  decreaseCount();
+                },
+                child: Container(
+                  width: 100,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(50),
+                    ),
+                    color: Colors.red,
+                  ),
+                  child: Center(
+                    child: Text(
+                      '-',
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                  ),
+                ),
+              )
+              /*FlatButton(
+                onPressed: () {},
+                child: Text('-'),
+                color: Colors.red,
+                textColor: Colors.white,
+              ),*/
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void increaseCount() {
+    setState(() {
+      number++;
+    });
+  }
+
+  void decreaseCount() {
+    setState(() {
+      number--;
+    });
   }
 }
