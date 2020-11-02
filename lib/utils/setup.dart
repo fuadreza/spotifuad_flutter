2cf2df11fa57a52d2ad46e0f62bfe64767b44307
fuadreza <fuadreza1011@gmail.com>
2020-11-02 11:22:31 +0700

    Feat: Add Splash Screen

---
+++ lib/utils/setup.dart
@@ -0,0 +1,10 @@
+const Settings = {
+  'PrimaryColor': '#20A286',
+  'AccentGrey': '#333435',
+  'DarkPrimary': '#1B5C4E',
+  'LightColor': '#D5EEE9',
+  'GreyColor': '#F0F0F0',
+  'DotColor': '#EEF5FF',
+  'StartGrad': '#F6D365',
+  'EndGrad': '#FDA085',
+};
